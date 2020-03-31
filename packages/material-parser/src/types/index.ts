9ce0a7362a91b9988bb60ca0baea449569cf93fa
gengyang <gengyang.gy@alibaba-inc.com>
2020-03-31 14:02:24 +0800

    fix: fix bug of missing types in material-parser

--- packages/material-parser/src/types/index.ts
+++ packages/material-parser/src/types/index.ts
@@ -4,19 +4,11 @@ import ExtensionName from './ExtensionName';
 import IAccesser from './IAccesser';
 import ICompiler from './ICompiler';
 import IExtensionConfigManifest from './IExtensionConfigManifest';
-import IGenerator from './IGenerator';
 import IMaterializeOptions from './IMaterializeOptions';
 import IMaterialScanModel from './IMaterialScanModel';
-import IParser from './IParser';
-import IScanner from './IScanner';
 import SourceType from './SourceType';
 
-export * from './IMaterialParsedModel';
-
 export {
-  IGenerator,
-  IParser,
-  IScanner,
   ExtensionName,
   IExtensionConfigManifest,
   IMaterializeOptions,
