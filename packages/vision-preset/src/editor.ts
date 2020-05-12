661d98dfbb5dc9d6170af40b99da1545470b71a3
wuyue.xht <wuyue.xht@alibaba-inc.com>
2020-05-12 21:12:55 +0800

    feat: 支持body和背景样式

--- packages/vision-preset/src/editor.ts
+++ packages/vision-preset/src/editor.ts
@@ -74,31 +74,47 @@ designer.addPropsReducer(upgradePropsReducer, TransformStage.Init);
 // 设计器组件样式处理
 function stylePropsReducer(props: any, node: any) {
   if (props && typeof props === 'object' && props.__style__) {
-    const doc = designer.currentDocument?.simulator?.contentDocument;
-    if (!doc) {
-      return;
-    }
     const cssId = '_style_pesudo_' + node.id.replace(/\$/g, '_');
     const cssClass = '_css_pesudo_' + node.id.replace(/\$/g, '_');
-    const dom = doc.getElementById(cssId);
-    if (dom) {
-      dom.parentNode?.removeChild(dom);
-    }
-    let styleProp = props.__style__;
-    if (typeof styleProp === 'object') {
-      styleProp = toCss(styleProp);
-    }
-    if (typeof styleProp === 'string') {
-      const s = doc.createElement('style');
-      props.className = cssClass;
-      s.setAttribute('type', 'text/css');
-      s.setAttribute('id', cssId);
-      doc.getElementsByTagName('head')[0].appendChild(s);
-
-      s.appendChild(doc.createTextNode(styleProp.replace(/:root/g, '.' + cssClass)));
-    }
+    const styleProp = props.__style__;
+    appendStyleNode(props, styleProp, cssClass, cssId);
+  }
+  if (props && typeof props === 'object' && props.pageStyle) {
+    const cssId = '_style_pesudo_engine-document';
+    const cssClass = 'engine-document';
+    const styleProp = props.pageStyle;
+    appendStyleNode(props, styleProp, cssClass, cssId);
+  }
+  if (props && typeof props === 'object' && props.containerStyle) {
+    const cssId = '_style_pesudo_' + node.id;
+    const cssClass = '_css_pesudo_' + node.id.replace(/\$/g, '_');
+    const styleProp = props.containerStyle;
+    appendStyleNode(props, styleProp, cssClass, cssId);
   }
   return props;
+}
+
+function appendStyleNode(props: any, styleProp: any, cssClass: string, cssId: string) {
+  const doc = designer.currentDocument?.simulator?.contentDocument;
+  if (!doc) {
+    return;
+  }
+  const dom = doc.getElementById(cssId);
+  if (dom) {
+    dom.parentNode?.removeChild(dom);
+  }
+  if (typeof styleProp === 'object') {
+    styleProp = toCss(styleProp);
+  }
+  if (typeof styleProp === 'string') {
+    const s = doc.createElement('style');
+    props.className = cssClass;
+    s.setAttribute('type', 'text/css');
+    s.setAttribute('id', cssId);
+    doc.getElementsByTagName('head')[0].appendChild(s);
+
+    s.appendChild(doc.createTextNode(styleProp.replace(/:root/g, '.' + cssClass)));
+  }
 }
 designer.addPropsReducer(stylePropsReducer, TransformStage.Render);
 
