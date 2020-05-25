a212480d62ba0592b75a74bb3488cc5bec557ab8
金禅 <mark.ck@alibaba-inc.com>
2020-05-25 21:09:34 +0800

    add assets

--- packages/material-parser/test/index.ts
+++ packages/material-parser/test/index.ts
@@ -1,89 +1,19 @@
-import test from 'ava';
 import parse from '../src';
+import fs from 'fs';
 import { IMaterializeOptions } from '../src/types';
 import { getFromFixtures } from './helpers';
 
-const multiExportedComptPath = getFromFixtures('multiple-exported-component');
-const singleExportedComptPath = getFromFixtures('single-exported-component');
 const fusionComptPath = getFromFixtures('fusion-next-component');
-const antdComptPath = getFromFixtures('antd-component');
-const singleExportedComponent = '@ali/demo-biz-test090702@0.0.2';
-const multipleExportedComponent = '@ali/aimake-basic@0.1.0';
-const tsComponent = getFromFixtures('ts-component');
 
-test('materialize single exported component by local', async t => {
-  const options: IMaterializeOptions = {
-    entry: singleExportedComptPath,
-    accesser: 'local',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
-
-test('materialize multiple exported component by local', async t => {
-  const options: IMaterializeOptions = {
-    entry: multiExportedComptPath,
-    accesser: 'local',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
-
-test.skip('materialize single exported component by online', async t => {
-  const options: IMaterializeOptions = {
-    entry: singleExportedComponent,
-    accesser: 'online',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
-
-test.skip('materialize multiple exported component by online', async t => {
-  const options: IMaterializeOptions = {
-    entry: multipleExportedComponent,
-    accesser: 'online',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
-
-test('ts component by local', async t => {
-  const options: IMaterializeOptions = {
-    entry: tsComponent,
-    accesser: 'local',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
-
-test('fusion next component by local', async t => {
+async function generate() {
   const options: IMaterializeOptions = {
     entry: fusionComptPath,
     accesser: 'local',
   };
 
   const actual = await parse(options);
+  fs.writeFileSync('configure.json', JSON.stringify(actual, null, 2));
+  console.log(actual);
+}
 
-  t.snapshot(actual);
-});
-
-test('antd component by local', async t => {
-  const options: IMaterializeOptions = {
-    entry: antdComptPath,
-    accesser: 'local',
-  };
-
-  const actual = await parse(options);
-
-  t.snapshot(actual);
-});
+generate();
