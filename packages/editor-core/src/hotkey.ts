3bd1248767f0f2f0cf20e412435a61b343f8a843
wuyue.xht <wuyue.xht@alibaba-inc.com>
2020-06-24 17:11:04 +0800

    fix: 修复删除时，当前组件信息丢失问题

--- packages/editor-core/src/hotkey.ts
+++ packages/editor-core/src/hotkey.ts
@@ -328,16 +328,16 @@ function getKeyInfo(combination: string, action?: string): KeyInfo {
  * convention - prevent default and stop propogation on the event
  */
 function fireCallback(callback: HotkeyCallback, e: KeyboardEvent, combo?: string, sequence?: string): void {
-  if (callback(e, combo) === false) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
   const editor = globalContext.get(Editor);
   const designer = editor.get('designer');
   const node = designer?.currentSelection?.getNodes()?.[0];
   const npm = node?.componentMeta?.npm;
   const selected =
     [npm?.package, npm?.componentName].filter((item) => !!item).join('-') || node?.componentMeta?.componentName || '';
+  if (callback(e, combo) === false) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
   editor?.emit('hotkey.callback.call', {
     callback,
     e,
