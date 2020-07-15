6330f219be98757a5fd3dabb1837653bcaf21ca2
YJSON <jiasong.yjs@alibaba-inc.com>
2020-07-15 17:51:09 +0800

    fix: 🐛 update start scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # FIXME! do not run build
-lerna exec --scope @ali/lowcode-react-renderer -- npm run build
-lerna exec --scope @ali/lowcode-demo -- npm start
+# lerna exec --scope @ali/lowcode-react-renderer -- npm run build
+# lerna exec --scope @ali/lowcode-demo -- npm start
+lerna exec --scope @ali/gdt-simple-form-sdk -- npm start
