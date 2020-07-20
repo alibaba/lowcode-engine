afda7d4259bb7f331b7faf42851d3e5f51feadb6
春希 <armslave.yy@alibaba-inc.com>
2020-07-20 22:24:00 +0800

    fix: 🐛 codeout btn fix

--- packages/demo/src/editor/plugins/codeout.tsx
+++ packages/demo/src/editor/plugins/codeout.tsx
@@ -2,11 +2,24 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import React from 'react';
 import { Button } from '@alifd/next';
-import { PluginProps } from '@ali/lowcode-types';
+import { PluginProps, NpmInfo } from '@ali/lowcode-types';
 import { Designer } from '@ali/lowcode-designer';
 import streamSaver from 'streamsaver';
 
 import './codeout.scss';
+
+interface BasicSection {
+  componentName: string;
+  title: string;
+  description?: string;
+  docUrl?: string;
+  screenshot?: string;
+  icon?: string;
+  tags?: string[];
+  devMode?: 'proCode' | 'lowCode';
+  npm: NpmInfo;
+  [k: string]: any;
+}
 
 const CODEOUT_SERVICE_HOST = '30.8.52.239:3000';
 
@@ -14,13 +27,15 @@ const Codeout = ({ editor }: PluginProps) => {
   const handleClick = () => {
     const designer = editor.get(Designer);
     if (designer) {
-      const assets = editor.get('assets');
-      console.log(assets.components);
+      const assets = editor.get('assets') as { components: BasicSection[] };
+      const components = assets.components;
 
-      const componentsMap = assets.components.map((c) => ({
-        componentName: c.componentName,
-        ...(c.npm || {}),
-      }));
+      const componentsMap = components
+        .filter((c) => !!c.npm)
+        .map((c) => ({
+          componentName: c.componentName,
+          ...(c.npm || {}),
+        }));
 
       const fullSchema = {
         ...designer.schema,
@@ -35,24 +50,26 @@ const Codeout = ({ editor }: PluginProps) => {
       };
 
       console.info('codeout schema:', fullSchema);
-      // fetch(`http://${CODEOUT_SERVICE_HOST}/api/generate/project`, {
-      //   method: 'POST',
-      //   body: JSON.stringify({ schema: JSON.stringify(fullSchema) }),
-      //   headers: new Headers({
-      //     'Content-Type': 'application/json',
-      //   }),
-      //   mode: 'cors',
-      // }).then((res) => {
-      //   const fileStream = streamSaver.createWriteStream('demoProject.zip');
-      //   res.body.pipeTo(fileStream).then(
-      //     () => {
-      //       console.log('success');
-      //     },
-      //     (err) => {
-      //       console.log(err);
-      //     },
-      //   );
-      // });
+      fetch(`http://${CODEOUT_SERVICE_HOST}/api/generate/project`, {
+        method: 'POST',
+        body: JSON.stringify({ schema: JSON.stringify(fullSchema) }),
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+        mode: 'cors',
+      }).then((res) => {
+        const fileStream = streamSaver.createWriteStream('demoProject.zip');
+        if (res.body !== null) {
+          res.body.pipeTo(fileStream).then(
+            () => {
+              console.log('success');
+            },
+            (err) => {
+              console.log(err);
+            },
+          );
+        }
+      });
     }
   };
 
