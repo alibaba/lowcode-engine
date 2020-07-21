d88ab7fe98b8c7a5c64ae717104041efa388f8b5
荣彬 <rongbin.arb@alibaba-inc.com>
2020-07-21 22:40:42 +0800

    修改rax-simulator-renderer 构建

--- packages/rax-simulator-renderer/src/obx-rax/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './shallow-equal';
-export * from './has-own-property';
-export * from './throttle';
-export * from './next-id';
-export * from './is-primitive';
-export * from './invariant';
-export * from './splitPath';
