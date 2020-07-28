18f149e0e8b336b2608e2c55f26ba0b796ceab59
wuji.xwt <wuji.xwt@alibaba-inc.com>
2020-07-28 14:08:34 +0800

    feat: plugin preview

--- packages/plugin-sample-preview/src/index.tsx
+++ packages/plugin-sample-preview/src/index.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import { Button } from '@alifd/next';
-import { PluginProps } from '@ali/lowcode-types';
-import { Designer } from '@ali/lowcode-designer';
+import React, { useState } from 'react';
+import { Button, Dialog } from '@alifd/next';
+// import { PluginProps } from '@ali/lowcode-types';
+// import { Designer } from '@ali/lowcode-designer';
+// import ReactRenderer from '@ali/lowcode-react-renderer';
 import './index.scss';
 
 const SamplePreview = ({ editor }: PluginProps) => {
-  const handleClick = () => {
+  const [data, setData] = useState({});
+  const [visible, setVisible] = useState(false);
+  const handleClick = async () => {
+    if (!editor) {
+      return;
+    }
     const designer = editor.get(Designer);
     if (designer) {
       console.info('save schema:', designer.schema);
-      localStorage.setItem('lce-dev-store', JSON.stringify(designer.schema));
-      fetch('http://30.5.157.206:3000/legao/save.json', {
-        method: 'POST',
-        body: JSON.stringify(designer.schema),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
-        mode: 'cors',
-      })
-        .then((res) => res.json())
-        .then(() => {
-          window.open('http://30.5.157.206:3333/', 'preview');
-        });
+
+      const { components } = await editor.get('assets');
+      setData({
+        schema: designer.schema,
+        components,
+      });
+      setVisible(true);
     }
   };
 
+  const { schema, components } = data;
   return (
     <div className="lowcode-plugin-sample-preview">
       <Button type="primary" onClick={handleClick}>
         预览
       </Button>
+      <Dialog
+        visible={visible}
+        footer={false}
+      >
+        <ReactRenderer 
+          schema={schema}
+          components={components}
+        />
+      </Dialog>
     </div>
   );
 };
