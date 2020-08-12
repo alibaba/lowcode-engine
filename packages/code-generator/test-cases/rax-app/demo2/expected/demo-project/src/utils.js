a1a3b68287b5de527863910737f8ef8185119a8c
牧毅 <changyun.pcy@alibaba-inc.com>
2020-08-13 00:35:29 +0800

    fix: 🐛 若全量引入 lodash 则在小程序下会跑不通,所以改成引入 lodash/clone

--- packages/code-generator/test-cases/rax-app/demo2/expected/demo-project/src/utils.js
+++ packages/code-generator/test-cases/rax-app/demo2/expected/demo-project/src/utils.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-import { clone } from 'lodash';
+import clone from 'lodash/clone';
 
 const formatPrice = function formatPrice(price, unit) {
   return Number(price).toFixed(2) + unit;
