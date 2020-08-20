c69f5fac43cf6d95dbbc38eb47a7144e5896d75d
力皓 <lihao.ylh@alibaba-inc.com>
2020-08-20 19:38:39 +0800

    chore: 兼容没有 currentScript 的情况

--- packages/editor-core/src/utils/get-public-path.ts
+++ packages/editor-core/src/utils/get-public-path.ts
@@ -1,5 +1,5 @@
-const publicPath = (document.currentScript as HTMLScriptElement).src.replace(/^(.*\/)[^/]+$/, '$1');
+const publicPath = (document.currentScript as HTMLScriptElement)?.src.replace(/^(.*\/)[^/]+$/, '$1');
 
 export function getPublicPath(): string {
-  return publicPath;
+  return publicPath || '';
 }
