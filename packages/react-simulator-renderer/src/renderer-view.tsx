0c4de43358c712368c82de7dcaa69ef9bb0f55f0
金禅 <mark.ck@alibaba-inc.com>
2020-09-01 19:11:09 +0800

    feat: 容器组件支持传入 placeholder 和对应样式

--- packages/react-simulator-renderer/src/renderer-view.tsx
+++ packages/react-simulator-renderer/src/renderer-view.tsx
@@ -117,17 +117,7 @@ class Renderer extends Component<{ renderer: SimulatorRenderer }> {
             (!viewProps.style || Object.keys(viewProps.style).length === 0)
           ) {
             children = (
-              <div
-                style={{
-                  height:'44px',
-                  lineHeight:'44px',
-                  backgroundColor: '#f0f0f0',
-                  borderColor: '#a7b1bd',
-                  border: '1px dotted',
-                  color: '#a7b1bd',
-                  textAlign: 'center'
-                }}
-              >
+              <div className="container-placeholder" style={viewProps.placeholderStyle}>
                 {viewProps.placeholder || '拖拽组件或模板到这里'}
               </div>
             );
