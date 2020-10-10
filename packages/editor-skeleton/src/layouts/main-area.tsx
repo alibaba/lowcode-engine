9816859c3f1de345377110faef0336fa9576b9fc
mario.gk <mario.gk@alibaba-inc.com>
2020-10-10 16:36:56 +0800

    fix: 样式微调

--- packages/editor-skeleton/src/layouts/main-area.tsx
+++ packages/editor-skeleton/src/layouts/main-area.tsx
@@ -14,7 +14,7 @@ export default class MainArea extends Component<{ area: Area<any, Panel | Widget
   render() {
     const { area } = this.props;
     return (
-      <div className={classNames('lc-main-area engine-workspacepane')}>
+      <div className={classNames('lc-main-area')}>
         {area.container.items.map((item) => item.content)}
       </div>
     );
