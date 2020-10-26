b7e413910719e9dcf7ff2418971cc1a35e256b38
力皓 <lihao.ylh@alibaba-inc.com>
2020-10-26 14:28:42 +0800

    chore: fix conflicts

--- packages/designer/tests/project/project.test.ts
+++ packages/designer/tests/project/project.test.ts
@@ -1,3 +1,5 @@
+import set from 'lodash.set';
+import cloneDeep from 'lodash.clonedeep';
 import '../fixtures/window';
 import { Project } from '../../src/project/project';
 // import { Node } from '../../../src/document/node/node';
@@ -30,39 +32,142 @@ beforeAll(() => {
   designer = new Designer({} as any);
 });
 
-describe('schema 渲染测试', () => {
-  it('最简单的例子，练手用', () => {
-    const project = new Project(designer, {
-      componentsTree: [{
-        componentName: 'Page',
-        id: 'page_id',
-        props: {
-          name: 'haha',
-        },
-        children: [{
-          componentName: 'Div',
-          id: 'div_id',
-          props: {
-            name: 'div from haha',
-          },
-        }],
-      }],
-    } as any);
-    project.open();
-    expect(project).toBeTruthy();
-    const { currentDocument } = project;
-    const { nodesMap } = currentDocument;
-    // console.log(project.currentDocument.nodesMap.get('div_id').props.items);
-    expect(nodesMap.has('page_id')).toBeTruthy;
-    expect(nodesMap.has('div_id')).toBeTruthy;
-    expect(mockCreateSettingEntry).toBeCalledTimes(2);
-    // console.log(currentDocument.export(3));
-  });
+describe('schema 生成节点模型测试', () => {
+  describe('block ❌ | component ❌ | slot ❌', () => {
+    it('基本的节点模型初始化，模型导出', () => {
+      const project = new Project(designer, {
+        componentsTree: [
+          formSchema,
+        ],
+      });
+      project.open();
+      expect(project).toBeTruthy();
+      const { currentDocument } = project;
+      const { nodesMap } = currentDocument;
+      const ids = getIdsFromSchema(formSchema);
+      const expectedNodeCnt = ids.length;
+      expect(nodesMap.size).toBe(expectedNodeCnt);
+      ids.forEach(id => {
+        expect(nodesMap.get(id).componentName).toBe(getNodeFromSchemaById(formSchema, id).componentName);
+      });
 
-  it.only('普通场景，无 block / component，无 slot', () => {
+      const exportSchema = currentDocument?.export(1);
+      expect(getIdsFromSchema(exportSchema).length).toBe(expectedNodeCnt);
+      expect(mockCreateSettingEntry).toBeCalledTimes(expectedNodeCnt);
+    });
+
+    describe('节点新增（insertNode）', () => {
+      let project;
+      beforeEach(() => {
+        project = new Project(designer, {
+          componentsTree: [
+            formSchema,
+          ],
+        });
+        project.open();
+      });
+      it.only('场景一：插入 NodeSchema', () => {
+        expect(project).toBeTruthy();
+        const ids = getIdsFromSchema(formSchema);
+        const { currentDocument } = project;
+        const { nodesMap } = currentDocument;
+        const formNode = nodesMap.get('node_k1ow3cbq');
+        currentDocument?.insertNode(formNode, {
+          componentName: 'TextInput',
+          id: 'nodeschema-id1',
+          props: {
+            propA: 'haha',
+            propB: 3
+          }
+        });
+        expect(nodesMap.get('nodeschema-id1').componentName).toBe('TextInput');
+        expect(nodesMap.size).toBe(ids.length + 1);
+      });
+
+      it.only('场景一：插入 NodeSchema，有 children', () => {
+        expect(project).toBeTruthy();
+        const ids = getIdsFromSchema(formSchema);
+        const { currentDocument } = project;
+        const { nodesMap } = currentDocument;
+        const formNode = nodesMap.get('node_k1ow3cbq');
+        currentDocument?.insertNode(formNode, {
+          componentName: 'TextInput',
+          id: 'nodeschema-id1',
+          props: {
+            propA: 'haha',
+            propB: 3
+          }
+        });
+        expect(nodesMap.get('nodeschema-id1').componentName).toBe('TextInput');
+        expect(nodesMap.size).toBe(ids.length + 1);
+      });
+
+      it.only('场景一：插入 NodeSchema，id 与现有 schema 重复', () => {
+        expect(project).toBeTruthy();
+        const ids = getIdsFromSchema(formSchema);
+        const { currentDocument } = project;
+        const { nodesMap } = currentDocument;
+        const formNode = nodesMap.get('node_k1ow3cbq');
+        currentDocument?.insertNode(formNode, {
+          componentName: 'TextInput',
+          id: 'nodeschema-id1',
+          props: {
+            propA: 'haha',
+            propB: 3
+          }
+        });
+        expect(nodesMap.get('nodeschema-id1').componentName).toBe('TextInput');
+        expect(nodesMap.size).toBe(ids.length + 1);
+      });
+
+      it.only('场景一：插入 NodeSchema，id 与现有 schema 重复，但关闭了 id 检测器', () => {
+        expect(project).toBeTruthy();
+        const ids = getIdsFromSchema(formSchema);
+        const { currentDocument } = project;
+        const { nodesMap } = currentDocument;
+        const formNode = nodesMap.get('node_k1ow3cbq');
+        currentDocument?.insertNode(formNode, {
+          componentName: 'TextInput',
+          id: 'nodeschema-id1',
+          props: {
+            propA: 'haha',
+            propB: 3
+          }
+        });
+        expect(nodesMap.get('nodeschema-id1').componentName).toBe('TextInput');
+        expect(nodesMap.size).toBe(ids.length + 1);
+      });
+
+      it('场景二：插入 Node 实例', () => {
+        expect(project).toBeTruthy();
+        const ids = getIdsFromSchema(formSchema);
+        const { currentDocument } = project;
+        const { nodesMap } = currentDocument;
+        const formNode = nodesMap.get('node_k1ow3cbq');
+        const inputNode = currentDocument?.createNode({
+          componentName: 'TextInput',
+          id: 'nodeschema-id2',
+          props: {
+            propA: 'haha',
+            propB: 3
+          }
+        });
+        expect(inputNode.id).toBe('nodeschema-id2');
+        currentDocument?.insertNode(formNode, inputNode);
+        expect(nodesMap.get('nodeschema-id2').componentName).toBe('TextInput');
+        expect(nodesMap.size).toBe(ids.length + 1);
+      });
+
+      it('场景二：插入 JSExpression', () => {});
+    });
+
+  })
+
+  it('block ❌ | component ❌ | slot ✅', () => {
+    const formSchemaWithSlot = set(cloneDeep(formSchema), 'children[0].children[0].props.title.type', 'JSSlot');
     const project = new Project(designer, {
       componentsTree: [
-        formSchema,
+        formSchemaWithSlot,
       ],
     });
     project.open();
@@ -70,17 +175,10 @@ describe('schema 渲染测试', () => {
     const { currentDocument } = project;
     const { nodesMap } = currentDocument;
     const ids = getIdsFromSchema(formSchema);
-    ids.forEach(id => {
-      expect(nodesMap.get(id).componentName).toBe(getNodeFromSchemaById(formSchema, id).componentName);
-    });
-    // console.log(nodesMap.get('node_k1ow3cb9').componentName, getNodeFromSchemaById(formSchema, 'node_k1ow3cb9').componentName)
-    console.log(nodesMap.size);
-    // expect(nodesMap.has('page_id')).toBeTruthy;
-    // expect(nodesMap.has('div_id')).toBeTruthy;
-    // expect(mockCreateSettingEntry).toBeCalledTimes(2);
-    // console.log(currentDocument.export(3));
+    // 目前每个 slot 会新增 1 + children.length 个节点
+    const expectedNodeCnt = ids.length + 2;
+    expect(nodesMap.size).toBe(expectedNodeCnt);
+    // PageHeader
+    expect(nodesMap.get('node_k1ow3cbd').slots).toHaveLength(1);
   });
-
-  it('普通场景，无 block / component，有 slot', () => {});
-  it('普通场景，无 block / component，有 slot', () => {});
 });
