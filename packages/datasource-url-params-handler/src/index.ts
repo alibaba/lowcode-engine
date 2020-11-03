af5ef187aa1b2cdfd3b028952c6c907cdfef7a23
guokai.jgk <guokai.jgk@alibaba-inc.com>
2020-11-04 00:10:50 +0800

    fix: lowcode types update

--- packages/datasource-url-params-handler/src/index.ts
+++ packages/datasource-url-params-handler/src/index.ts
@@ -1,7 +1,10 @@
 import qs from 'query-string';
 import { UrlParamsHandler } from '@ali/lowcode-types';
 
-export function createUrlParamsHandler<T = unknown>(searchString: string | T = ''): UrlParamsHandler<T> {
+export function createUrlParamsHandler<T = unknown>(
+  searchString: string | T = '',
+): UrlParamsHandler<T> {
+  // eslint-disable-next-line space-before-function-paren
   return async function(): Promise<T> {
     if (typeof searchString === 'string') {
       const params = (qs.parse(searchString) as unknown) as T;
