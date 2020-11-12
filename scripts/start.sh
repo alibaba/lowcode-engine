05374954863c5e783bd12db16e383772ef2f85e9
guokai.jgk <guokai.jgk@alibaba-inc.com>
2020-11-12 17:15:16 +0800

    fix: start shell add datasource build

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # FIXME! do not run build
+lerna exec --scope @ali/lowcode-datasource-engine -- npm run build
 lerna exec --scope @ali/lowcode-rax-renderer -- npm run build
 lerna exec --scope @ali/lowcode-react-renderer -- npm run build
 lerna exec --scope @ali/lowcode-demo -- npm start
