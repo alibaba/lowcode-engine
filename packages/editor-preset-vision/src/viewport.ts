4bc4ddec9d5c529b8719ded2aacb5de1af6be203
力皓 <lihao.ylh@alibaba-inc.com>
2020-12-03 20:08:24 +0800

    fix: 修复 setDevice 的时机，从 currentDocument -> simualtor fix: 根据 canDropIn 来支持选择 Page 节点时是否可添加组件

--- packages/editor-preset-vision/src/viewport.ts
+++ packages/editor-preset-vision/src/viewport.ts
@@ -195,8 +195,8 @@ export class Viewport {
   async setDevice(device = 'pc') {
     if (this.getDevice() !== device) {
       this.device = device;
-      const currentDocument = await editor.onceGot('currentDocuemnt');
-      currentDocument?.simulator?.set('device', device === 'mobile' ? 'mobile' : 'default');
+      const simulator = await editor.onceGot('simulator');
+      simulator?.set('device', device === 'mobile' ? 'mobile' : 'default');
       // Flags.setSimulator(device);
       // this.applyMediaCSS();
       this.emitter.emit('devicechange', device);
