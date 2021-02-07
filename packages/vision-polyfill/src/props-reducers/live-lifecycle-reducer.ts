b295da1a021ec995ebfe1573cb0482edc511c4db
力皓 <lihao.ylh@alibaba-inc.com>
2021-02-07 10:57:28 +0800

    fix: lifecycle reducer 跳过非 vc 的组件

--- packages/vision-polyfill/src/props-reducers/live-lifecycle-reducer.ts
+++ packages/vision-polyfill/src/props-reducers/live-lifecycle-reducer.ts
@@ -2,6 +2,10 @@ import { editor } from '@ali/lowcode-engine';
 import { Node } from '@ali/lowcode-designer';
 
 export function liveLifecycleReducer(props: any, node: Node) {
+  // 如果不是 vc 体系，不做这个兼容处理
+  if (!node.componentMeta.prototype) {
+    return props;
+  }
   // live 模式下解析 lifeCycles
   if (node.isRoot() && props && props.lifeCycles) {
     if (editor.get('designMode') === 'live') {
