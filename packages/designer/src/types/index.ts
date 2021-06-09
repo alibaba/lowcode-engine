1297e3c0e5f13a50bdd6e79ce5da37a8272e1ff7
力皓 <lihao.ylh@alibaba-inc.com>
2021-06-09 14:57:52 +0800

    fix: 修复 valueChange 不会因为子属性变化而通知父属性事件监听, 考虑到后续推荐直接使用 setValue, 也实现了 valueChange 事件

--- packages/designer/src/types/index.ts
+++ packages/designer/src/types/index.ts
@@ -19,3 +19,20 @@ const utils = {
   getNodeSchemaById,
 };
 export type Utils = typeof utils;
+
+export enum PROP_VALUE_CHANGED_TYPE {
+  /**
+   * normal set value
+   */
+  SET_VALUE = 'SET_VALUE',
+  /**
+   * value changed caused by sub-prop value change
+   */
+  SUB_VALUE_CHANGE = 'SUB_VALUE_CHANGE',
+}
+
+export interface ISetValueOptions {
+  disableMutator?: boolean;
+  type?: PROP_VALUE_CHANGED_TYPE;
+  fromSetHotValue?: boolean;
+}
