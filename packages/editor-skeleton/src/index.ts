87c838231799df66d94678e350e6476c7dbd32ee
zude.hzd <zude.hzd@alibaba-inc.com>
2021-08-16 11:21:18 +0800

    feat: 从editor-skeleton中导出popup,便于array-setter和object-setter拆分

--- packages/editor-skeleton/src/index.ts
+++ packages/editor-skeleton/src/index.ts
@@ -3,5 +3,6 @@ export * from './skeleton';
 export * from './types';
 export * from './components/settings';
 export * from './components/field';
+export * from './components/popup';
 export * from './context';
 export * from './register-defaults';
