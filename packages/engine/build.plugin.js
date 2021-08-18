b997917ff35a608ddf672d7c4535e0cb43bfe301
lihao.ylh <lihao.ylh@alibaba-inc.com>
2021-08-18 15:40:39 +0800

    fix: 修改生成引擎版本号以及 engine / vision polyfill sourceMap 的 bug

--- packages/engine/build.plugin.js
+++ packages/engine/build.plugin.js
@@ -7,18 +7,7 @@ module.exports = ({ onGetWebpackConfig }) => {
       .use(TsconfigPathsPlugin, [{
         configFile: './tsconfig.json',
       }]);
-
-    /*
-    config
-      // 定义插件名称
-      .plugin('MonacoWebpackPlugin')
-      // 第一项为具体插件，第二项为插件参数
-      .use(new MonacoWebpackPlugin({
-        languages:["javascript","css","json"]
-      }), []);
-      */
     config.plugins.delete('hot');
     config.devServer.hot(false);
-    config.devtool('hidden-source-map');
   });
 };
