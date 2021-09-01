3f2cd667d0fea6c741fbdc449d512650350e20ae
lihao.ylh <lihao.ylh@alibaba-inc.com>
2021-09-01 21:10:02 +0800

    refactor: prop 的子实例不再延迟初始化, 也不再 node 未初始化完成之前就调用 dispose 方法

--- packages/designer/src/document/node/node.ts
+++ packages/designer/src/document/node/node.ts
@@ -157,7 +157,7 @@ export class Node<Schema extends NodeSchema = NodeSchema> {
     return this.componentMeta.icon;
   }
 
-  private isInited = false;
+  isInited = false;
 
   constructor(readonly document: DocumentModel, nodeSchema: Schema, options: any = {}) {
     const { componentName, id, children, props, ...extras } = nodeSchema;
@@ -168,9 +168,6 @@ export class Node<Schema extends NodeSchema = NodeSchema> {
         children: isDOMText(children) || isJSExpression(children) ? children : '',
       });
     } else {
-      // 这里 props 被初始化两次，一次 new，一次 import，new 的实例需要给 propsReducer 的钩子去使用，
-      // import 是为了使用钩子返回的值，并非完全幂等的操作，部分行为执行两次会有 bug，
-      // 所以在 props 里会对 new / import 做一些区别化的解析
       this.props = new Props(this, props, extras);
       this._children = new NodeChildren(this as ParentalNode, this.initialChildren(children));
       this._children.internalInitParent();
