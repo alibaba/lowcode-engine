6fad8fb86a7ba2e049a3dad0971fc5533e430589
lihao.ylh <lihao.ylh@alibaba-inc.com>
2021-09-28 20:48:26 +0800

    chore: 增加 setup-for-test

--- scripts/setup-for-test.sh
+++ scripts/setup-for-test.sh
@@ -12,7 +12,4 @@ lerna exec --stream \
   --scope @ali/lowcode-editor-core \
   --scope @ali/lowcode-types \
   --scope @ali/lowcode-utils \
-  --scope @ali/lowcode-renderer-core \
-  --scope @ali/lowcode-react-renderer \
-  --scope @ali/lowcode-rax-renderer \
   -- npm run build
