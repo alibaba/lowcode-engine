c94a4fb1df495093160636ac4230904ff3be8f7d
牧毅 <changyun.pcy@alibaba-inc.com>
2021-12-20 17:07:38 +0800

    chore: 去掉一些lerna 项目中不需要的文件

--- packages/code-generator/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: 'eslint-config-ali/typescript/react',
-  rules: {
-    'max-len': ['error', { code: 200 }],
-    'function-paren-newline': 'off',
-    '@typescript-eslint/indent': 'off',
-  },
-};
--- packages/code-generator/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['/node_modules/(?!core-js)/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**', '!**/vendor/**'],
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
-};
