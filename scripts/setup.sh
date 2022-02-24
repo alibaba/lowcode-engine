1cfb15aebea9796af23b2135f2aa4409d81283d7
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-02-24 11:23:27 +0800

    fix: 修复 setup 的逻辑

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-#tnpm i -g lerna @ali/tyarn
-
-rm -rf node_modules package-lock.json yarn.lock
+rm -rf package-lock.json yarn.lock
 lerna clean -y
 find ./packages -type f -name "package-lock.json" -exec rm -f {} \;
 
