168ed4182b16eee271236dc04766e58af864aa2f
humphry.hy <humphry.hy@alibaba-inc.com>
2022-03-01 16:03:37 +0800

    chore: remove duplicate usage of npm install lerna

--- scripts/setup-skip-build.sh
+++ scripts/setup-skip-build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-npm i -g lerna @ali/tyarn
-
 rm -rf node_modules package-lock.json yarn.lock
 lerna clean -y
 find ./packages -type f -name "package-lock.json" -exec rm -f {} \;
