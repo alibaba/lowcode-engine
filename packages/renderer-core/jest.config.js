d5daba8aaf1f782acc8a74c66c0191dbfed0e0d9
JackLian <jack.lianjie@gmail.com>
2022-04-18 18:28:57 +0800

    test: add ut for render-core/utils/common

--- packages/renderer-core/jest.config.js
+++ packages/renderer-core/jest.config.js
@@ -1,24 +1,41 @@
-module.exports = {
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|ts|tsx|jsx)$': 'babel-jest',
-    '^.+\\.(css|less|scss)$': './test/mock/styleMock.js',
-  },
-  // testMatch: ['**/bugs/*.test.ts'],
+const fs = require('fs');
+const { join } = require('path');
+const esModules = ['zen-logger'].join('|');
+const pkgNames = fs.readdirSync(join('..')).filter(pkgName => !pkgName.startsWith('.'));
+
+const jestConfig = {
+  // transform: {
+  //   // '^.+\\.[jt]sx?$': 'babel-jest',
+  //   '^.+\\.(ts|tsx)$': 'ts-jest',
+  //   // '^.+\\.(js|jsx)$': 'babel-jest',
+  // },
   // testMatch: ['(/tests?/.*(test))\\.[jt]s$'],
-  // transformIgnorePatterns: [
-  //   `/node_modules/(?!${esModules})/`,
-  // ],
-  testEnvironment: 'jsdom',
+  // testMatch: ['**/*/common.test.ts'],
+  transformIgnorePatterns: [
+    `/node_modules/(?!${esModules})/`,
+  ],
+  setupFiles: ['./tests/fixtures/unhandled-rejection.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   collectCoverage: false,
   collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-  ],
-  moduleNameMapper: {
-    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-  },
-  setupFilesAfterEnv: [
-    './test/setup.ts',
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/icons/**',
+    '!src/locale/**',
+    '!src/builtin-simulator/utils/**',
+    '!src/plugin/sequencify.ts',
+    '!src/document/node/exclusive-group.ts',
+    '!src/document/node/props/value-to-source.ts',
+    '!src/builtin-simulator/live-editing/live-editing.ts',
+    '!src/designer/offset-observer.ts',
+    '!src/designer/clipboard.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
   ],
 };
+
+// 只对本仓库内的 pkg 做 mapping
+jestConfig.moduleNameMapper = {};
+jestConfig.moduleNameMapper[`^@alilc/lowcode\\-(${pkgNames.join('|')})$`] = '<rootDir>/../$1/src';
+
+module.exports = jestConfig;
