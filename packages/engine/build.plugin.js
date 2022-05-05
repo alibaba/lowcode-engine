03041617957cf9329d8076da4532330f9f048d2c
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-05-05 17:20:15 +0800

    chore: get release version from git branch name (support beta identifier)

--- packages/engine/build.plugin.js
+++ packages/engine/build.plugin.js
@@ -1,18 +1,26 @@
 const { execSync } = require('child_process');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const fse = require('fs-extra');
 
-function getReleaseVersion() {
+// get version from git branch name,
+//  e.g. release/1.0.7 => 1.0.7
+//       release/1.0.7-beta => 1.0.7 (beta)
+function getVersion() {
   const gitBranchName = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' });
-  const reBranchVersion = /^(?:[\w-]+\/)(\d+\.\d+\.\d+)$/im;
+  const reBranchVersion = /^(?:[\w-]+\/)(\d+\.\d+\.\d+)(-?beta)?$/im;
+
   const match = reBranchVersion.exec(gitBranchName);
   if (!match) {
-    throw new Error(`[engine] gitBranchName: ${gitBranchName} is not valid`);
+    console.warn(`[engine] gitBranchName: ${gitBranchName}`);
+    return 'N/A';
   }
 
-  return match[1];
+  const [_, version, beta] = match;
+
+  return beta && beta.endsWith('beta') ? `${version}(beta)` : version;
 }
 
-const version = getReleaseVersion();
+const releaseVersion = getVersion();
 
 module.exports = ({ context, onGetWebpackConfig }) => {
   onGetWebpackConfig((config) => {
@@ -24,7 +32,7 @@ module.exports = ({ context, onGetWebpackConfig }) => {
     config
       .plugin('define')
       .use(context.webpack.DefinePlugin, [{
-        VERSION_PLACEHOLDER: JSON.stringify(version),
+        VERSION_PLACEHOLDER: JSON.stringify(releaseVersion),
       }]);
     config.plugins.delete('hot');
     config.devServer.hot(false);
