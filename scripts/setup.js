5006b5855ffa0bea9c46ee377db8179ec2ccb295
blueju <blueju@foxmail.com>
2022-05-31 07:21:39 +0000

    chore: npm run setup 命令适配 win 系统

---
+++ scripts/setup.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const os = require('os');
+const del = require('del');
+const gulp = require('gulp');
+const shell = require('shelljs');
+
+async function deleteRootDirLockFile() {
+    await del('package-lock.json');
+    await del('yarn.lock');
+}
+
+async function clean() {
+    await shell.exec('lerna clean -y');
+}
+
+async function deletePackagesDirLockFile() {
+    await del('packages/**/package-lock.json');
+}
+
+async function bootstrap() {
+    shell.exec('lerna bootstrap --force-local');
+}
+
+const setup = gulp.series(deleteRootDirLockFile, clean, deletePackagesDirLockFile, bootstrap);
+
+os.type() === 'Windows_NT' ? setup() : shell.exec('scripts/setup.sh');
