f13c3485a9a8c3dec9f8d2277248d7502bb01556
blueju <blueju@foxmail.com>
2022-06-01 04:00:49 +0000

    chore: 调整样式

--- scripts/start.js
+++ scripts/start.js
@@ -4,12 +4,7 @@ const execa = require('execa');
 
 async function start() {
     const [, , pkgName = '@alilc/lowcode-ignitor'] = process.argv;
-    await execa.command(`lerna exec --scope ${pkgName} -- npm start`, {
-        stdio: 'inherit',
-        encoding: 'utf-8',
-    });
+    await execa.command(`lerna exec --scope ${pkgName} -- npm start`, { stdio: 'inherit', encoding: 'utf-8' });
 }
 
-os.type() === 'Windows_NT'
-    ? start()
-    : execa.command('scripts/start.sh', { stdio: 'inherit', encoding: 'utf-8' });
+os.type() === 'Windows_NT' ? start() : execa.command('scripts/start.sh', { stdio: 'inherit', encoding: 'utf-8' });
