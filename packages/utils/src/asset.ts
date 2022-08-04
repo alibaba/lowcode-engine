141e19593184d18cb9cac7f7a62b8188059eac4e
刘菊萍(絮黎) <liujup@foxmail.com>
2022-08-04 18:25:42 +0800

    Revert "fix: delete unused typescript types"
    This reverts commit 2432aed83d55407d2f8b5f94910ada7ea78bb59e.

--- packages/utils/src/asset.ts
+++ packages/utils/src/asset.ts
@@ -1,11 +1,10 @@
-import { AssetType, AssetLevels, AssetLevel } from '@alilc/lowcode-types';
-import type { AssetItem, Asset, AssetList, AssetBundle, AssetsJson } from '@alilc/lowcode-types';
+import { AssetItem, AssetType, AssetLevels, Asset, AssetList, AssetBundle, AssetLevel, AssetsJson } from '@alilc/lowcode-types';
 import { isCSSUrl } from './is-css-url';
 import { createDefer } from './create-defer';
 import { load, evaluate } from './script';
 
 // API 向下兼容
-export { AssetType, AssetLevels, AssetLevel } from '@alilc/lowcode-types';
+export { AssetItem, AssetType, AssetLevels, Asset, AssetList, AssetBundle, AssetLevel, AssetsJson } from '@alilc/lowcode-types';
 
 export function isAssetItem(obj: any): obj is AssetItem {
   return obj && obj.type;
