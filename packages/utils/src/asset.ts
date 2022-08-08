bcf9aeea0385053c2f931b1637b3d532764d256d
zhangjinmei <1075487618@qq.com>
2022-08-08 12:49:21 +0800

    fix: change typescript type export to export type

--- packages/utils/src/asset.ts
+++ packages/utils/src/asset.ts
@@ -1,16 +1,16 @@
-import { AssetItem, AssetType, AssetLevels, Asset, AssetList, AssetBundle, AssetLevel, AssetsJson } from '@alilc/lowcode-types';
+import { AssetType, AssetLevels, AssetLevel } from '@alilc/lowcode-types';
+import type { AssetItem, Asset, AssetList, AssetBundle, AssetsJson } from '@alilc/lowcode-types';
 import { isCSSUrl } from './is-css-url';
 import { createDefer } from './create-defer';
 import { load, evaluate } from './script';
 
 // API 向下兼容
-export { AssetType, AssetLevels, Asset, AssetList, AssetBundle, AssetLevel, AssetsJson } from '@alilc/lowcode-types';
+export { AssetType, AssetLevels, AssetLevel } from '@alilc/lowcode-types';
+export type { AssetItem, Asset, AssetList, AssetBundle, AssetsJson } from '@alilc/lowcode-types';
 
 export function isAssetItem(obj: any): obj is AssetItem {
   return obj && obj.type;
 }
-
-export type { AssetItem } from '@alilc/lowcode-types';
 
 export function isAssetBundle(obj: any): obj is AssetBundle {
   return obj && obj.type === AssetType.Bundle;
