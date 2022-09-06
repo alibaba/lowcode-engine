1f355c6a58348a1abd34abb6a64ac982d11dcfe0
akirakai <doriru.simon@gmail.com>
2022-09-06 11:23:28 +0800

    feat: update snapshots

---
+++ modules/material-parser/test/fixtures/transpiled-component/src/index.js
@@ -0,0 +1,111 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _next = require("@alifd/next");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+/**
+ * General component description.
+ */
+var MyComponent = /*#__PURE__*/function (_Component) {
+  _inherits(MyComponent, _Component);
+
+  var _super = _createSuper(MyComponent);
+
+  function MyComponent() {
+    _classCallCheck(this, MyComponent);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(MyComponent, [{
+    key: "add",
+
+    /**
+     * adds two numbers.
+     * @constructor
+     * @param {number} x - 1st number.
+     * @param {number} y - 2nd number.
+     */
+    value: function add(x, y) {
+      var ret = x + y;
+      return ret;
+    }
+  }, {
+    key: "render",
+    value: function render() {// ...
+    }
+  }]);
+
+  return MyComponent;
+}(_react.Component);
+
+function AComponent(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/_react["default"].createElement("div", null, children);
+}
+
+MyComponent.propTypes = {
+  /**
+   * Description of prop "foo".
+   */
+  foo: _propTypes["default"].number.isRequired,
+
+  /**
+   * Description of prop "bar" (a custom validation function).
+   */
+  bar: function bar(props, propName, componentName) {// ...
+  },
+  baz: _propTypes["default"].oneOfType([_propTypes["default"].number, _propTypes["default"].string]),
+  optionalObjectWithShape2: _propTypes["default"].shape({
+    optionalProperty: _propTypes["default"].string,
+    requiredProperty: _propTypes["default"].number.isRequired
+  }).isRequired // ...Button.propTypes
+
+};
+MyComponent.defaultProps = {
+  bar: 21,
+
+  get foo() {
+    return 1 + 1;
+  }
+
+};
+MyComponent.AComponent = AComponent;
+var _default = MyComponent;
+exports["default"] = _default;
