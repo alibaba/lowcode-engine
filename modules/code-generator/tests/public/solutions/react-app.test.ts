12d1291113f05aa22988bf8036eb5e280bcbb09c
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-11-24 15:59:22 +0800

    refactor: move test-cases to tests/fixtures folder

--- modules/code-generator/tests/public/solutions/react-app.test.ts
+++ modules/code-generator/tests/public/solutions/react-app.test.ts
@@ -16,7 +16,7 @@ import type { ProjectSchema } from '@alilc/lowcode-types';
 
 jest.setTimeout(15 * 1000);
 
-const TEST_CASES_DIR = path.join(__dirname, '../../../test-cases/react-app');
+const TEST_CASES_DIR = path.join(__dirname, '../../fixtures/test-cases/react-app');
 const SHOULD_UPDATE_EXPECTED = process.env.UPDATE_EXPECTED === 'true';
 
 getSubDirectoriesSync(TEST_CASES_DIR).forEach(defineTest);
