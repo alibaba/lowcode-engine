a079fbc256f8275e8a69eb6d8abb6f6b08179578
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-11-24 14:02:45 +0800

    fix: 修复生成的 icejs 项目不支持 constants 的问题, fixes #1259

---
+++ modules/code-generator/src/plugins/component/react/containerInjectConstants.ts
@@ -0,0 +1,55 @@
+import {
+  CLASS_DEFINE_CHUNK_NAME,
+  COMMON_CHUNK_NAME,
+  DEFAULT_LINK_AFTER,
+} from '../../../const/generator';
+
+import {
+  BuilderComponentPlugin,
+  BuilderComponentPluginFactory,
+  ChunkType,
+  FileType,
+  ICodeStruct,
+} from '../../../types';
+
+export interface PluginConfig {
+  fileType: string;
+}
+
+const pluginFactory: BuilderComponentPluginFactory<PluginConfig> = (config?) => {
+  const cfg: PluginConfig = {
+    fileType: FileType.JSX,
+    ...config,
+  };
+
+  const plugin: BuilderComponentPlugin = async (pre: ICodeStruct) => {
+    const next: ICodeStruct = {
+      ...pre,
+    };
+
+    next.chunks.push({
+      type: ChunkType.STRING,
+      fileType: cfg.fileType,
+      name: COMMON_CHUNK_NAME.InternalDepsImport,
+      content: "import __$$constants from '../../constants';",
+      linkAfter: [COMMON_CHUNK_NAME.ExternalDepsImport],
+    });
+
+    next.chunks.push({
+      type: ChunkType.STRING,
+      fileType: cfg.fileType,
+      name: CLASS_DEFINE_CHUNK_NAME.InsVar,
+      content: `
+        get constants() {
+          return __$$constants || {};
+        }
+        `,
+      linkAfter: [...DEFAULT_LINK_AFTER[CLASS_DEFINE_CHUNK_NAME.InsVar]],
+    });
+
+    return next;
+  };
+  return plugin;
+};
+
+export default pluginFactory;
