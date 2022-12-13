d1e08037d44d01c1aaa87af4f56e94cf880441d7
JackLian <jack.lianjie@gmail.com>
2022-12-13 17:18:37 +0800

    fix: remove as Type

--- packages/editor-core/src/utils/assets-transform.ts
+++ packages/editor-core/src/utils/assets-transform.ts
@@ -1,25 +1,27 @@
-import { AssetsJson, ComponentDescription } from '@alilc/lowcode-types';
+/* eslint-disable no-param-reassign */
+import { AssetsJson, ComponentDescription, Package, RemoteComponentDescription } from '@alilc/lowcode-types';
 
-
+// TODO: 该转换逻辑未来需要消化掉
 export function assetsTransform(assets: AssetsJson) {
   const { components, packages } = assets;
-  const packageMaps = (packages || []).reduce((acc, cur) => {
-    const key = (cur.id || cur.package) as string;
+  const packageMaps = (packages || []).reduce((acc: Record<string, Package>, cur: Package) => {
+    const key = cur.id || cur.package || '';
     acc[key] = cur;
     return acc;
   }, {} as any);
-  components.forEach((componentDesc) => {
-    let { devMode, schema, reference } = componentDesc as ComponentDescription;
+  components.forEach((componentDesc: ComponentDescription | RemoteComponentDescription) => {
+    let { devMode, schema, reference } = componentDesc;
     if ((devMode as string) === 'lowcode') {
       devMode = 'lowCode';
     } else if (devMode === 'proCode') {
       devMode = 'proCode';
     }
     if (devMode) {
-      (componentDesc as ComponentDescription).devMode = devMode;
+      componentDesc.devMode = devMode;
     }
     if (devMode === 'lowCode' && !schema && reference) {
-      (componentDesc as ComponentDescription).schema = packageMaps[reference.id as string].schema;
+      const referenceId = reference.id || '';
+      componentDesc.schema = packageMaps[referenceId].schema;
     }
   });
   return assets;
