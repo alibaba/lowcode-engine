8958f1739a18d50b46f5c62b906428c198116432
liujuping <liujup@foxmail.com>
2022-12-22 15:53:41 +0800

    fix: fix responsive invalidation caused by upgrading build-plugin-component

--- packages/engine/build.plugin.js
+++ packages/engine/build.plugin.js
@@ -25,6 +25,25 @@ const releaseVersion = getVersion();
 
 module.exports = ({ context, onGetWebpackConfig }) => {
   onGetWebpackConfig((config) => {
+    ['jsx', 'tsx'].forEach((rule) => {
+      config.module
+      .rule(rule)
+      .exclude.clear()
+      .add(/node_modules(?!(.+_component_demo|.+build-plugin-component))/)
+      .end()
+      .use('babel-loader')
+      .tap((options) => {
+        const { plugins = [] } = options;
+        console.log('plugins', plugins);
+        return {
+          ...options,
+          plugins: [
+            ...plugins,
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+          ],
+        };
+      });
+    });
     config.resolve
       .plugin('tsconfigpaths')
       .use(TsconfigPathsPlugin, [{
