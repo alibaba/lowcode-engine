840e70e3d01a91e63d0e628c3c14820148bc1f4b
liujuping <liujup@foxmail.com>
2022-12-26 12:07:19 +0800

    docs: add workspace docs

---
+++ packages/types/src/shell/type/resource-options.ts
@@ -0,0 +1,32 @@
+export interface IPublicViewFunctions {
+  /** 视图初始化 */
+  init?: () => Promise<void>;
+  /** 资源保存时调用视图的钩子 */
+  save?: () => Promise<void>;
+}
+
+export interface IPublicEditorView {
+  /** 资源名字 */
+  viewName: string;
+  (ctx: any): IPublicViewFunctions;
+}
+
+export interface IPublicResourceOptions {
+  /** 资源名字 */
+  name: string;
+
+  /** 资源描述 */
+  description?: string;
+
+  /** 默认视图类型 */
+  defaultViewType: string;
+
+  /** 资源视图 */
+  editorViews: IPublicEditorView[];
+
+  /** save 钩子 */
+  save?: () => Promise<void>;
+
+  /** import 钩子 */
+  import?: () => Promise<void>;
+}
