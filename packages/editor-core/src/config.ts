27c1a69a3a0c30022344447250095ef14a58846c
za-liyong002 <liyong002@zhongan.io>
2023-02-25 19:32:58 +0800

    fix:修复config onGot 第一次获取到值之后，之后不会继续订阅的问题。

--- packages/editor-core/src/config.ts
+++ packages/editor-core/src/config.ts
@@ -291,14 +291,13 @@ export class EngineConfig implements IPublicModelEngineConfig, IEngineConfigPriv
     const val = this.config?.[key];
     if (val !== undefined) {
       fn(val);
-      return () => {};
-    } else {
-      this.setWait(key, fn);
-      return () => {
-        this.delWait(key, fn);
-      };
     }
+    this.setWait(key, fn);
+    return () => {
+      this.delWait(key, fn);
+    };
   }
+
 
   notifyGot(key: string): void {
     let waits = this.waits.get(key);
