b204bd62ad0d129d979eb16427d346d9db881f7e
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2023-03-24 16:34:05 +0800

    chore: test cr job

--- packages/designer/src/designer/active-tracker.ts
+++ packages/designer/src/designer/active-tracker.ts
@@ -14,6 +14,7 @@ export interface IActiveTracker extends Omit< IPublicModelActiveTracker, 'track'
 
 export interface ActiveTarget extends Omit< IPublicTypeActiveTarget, 'node' > {
   node: INode;
+  x: string;
 }
 
 export class ActiveTracker implements IActiveTracker {
