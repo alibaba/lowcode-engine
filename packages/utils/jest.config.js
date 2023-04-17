6160056139a044e6b32b2ab0573694fef7f38453
liujuping <liujup@foxmail.com>
2023-04-17 11:31:19 +0800

    fix(utils): isReactComponent not including react.memo

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -1,9 +1,20 @@
-module.exports = {
+const fs = require('fs');
+const { join } = require('path');
+const pkgNames = fs.readdirSync(join('..')).filter(pkgName => !pkgName.startsWith('.'));
+
+const jestConfig = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
     '!**/node_modules/**',
     '!**/vendor/**',
   ],
 };
+
+// 只对本仓库内的 pkg 做 mapping
+jestConfig.moduleNameMapper = {};
+jestConfig.moduleNameMapper[`^@alilc/lowcode\\-(${pkgNames.join('|')})$`] = '<rootDir>/../$1/src';
+
+module.exports = jestConfig;
