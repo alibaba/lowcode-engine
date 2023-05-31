e23bda1a104f14e07cea289cd17331b44e7e4e34
knight.chen <knightchen@knx.com.cn>
2023-05-31 22:48:49 +0800

    feat: extract simulator type

--- packages/designer/src/builtin-simulator/renderer.ts
+++ packages/designer/src/builtin-simulator/renderer.ts
@@ -1,29 +1,7 @@
 import { Component } from '../simulator';
-import { IPublicTypeComponentInstance, IPublicTypeNodeInstance, Asset, IPublicTypeComponentSchema, IPublicTypeProjectSchema, IPublicTypeLowCodeComponent } from '@alilc/lowcode-types';
+import { IPublicTypeComponentInstance, IPublicTypeSimulatorRenderer } from '@alilc/lowcode-types';
 
-export interface BuiltinSimulatorRenderer {
-  readonly isSimulatorRenderer: true;
-  autoRepaintNode?: boolean;
-  components: Record<string, Component>;
-  rerender: () => void;
-  createComponent(schema: IPublicTypeProjectSchema<IPublicTypeComponentSchema>): Component | null;
-  getComponent(componentName: string): Component;
-  getClosestNodeInstance(
-      from: IPublicTypeComponentInstance,
-      nodeId?: string,
-    ): IPublicTypeNodeInstance<IPublicTypeComponentInstance> | null;
-  findDOMNodes(instance: IPublicTypeComponentInstance): Array<Element | Text> | null;
-  getClientRects(element: Element | Text): DOMRect[];
-  setNativeSelection(enableFlag: boolean): void;
-  setDraggingState(state: boolean): void;
-  setCopyState(state: boolean): void;
-  loadAsyncLibrary(asyncMap: { [index: string]: any }): void;
-  clearState(): void;
-  stopAutoRepaintNode(): void;
-  enableAutoRepaintNode(): void;
-  run(): void;
-  load(asset: Asset): Promise<any>;
-}
+export type BuiltinSimulatorRenderer = IPublicTypeSimulatorRenderer<Component, IPublicTypeComponentInstance>;
 
 export function isSimulatorRenderer(obj: any): obj is BuiltinSimulatorRenderer {
   return obj && obj.isSimulatorRenderer;
