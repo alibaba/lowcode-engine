c838dc70eb71e4aaae37494baf7f3a1d3fec8340
liujuping <liujup@foxmail.com>
2023-06-12 11:29:22 +0800

    feat(workspace): add editorViews to resourceTypeList api

--- packages/types/src/shell/model/editor-view.ts
+++ packages/types/src/shell/model/editor-view.ts
@@ -1,3 +1,7 @@
 import { IPublicModelPluginContext } from './plugin-context';
 
-export interface IPublicModelEditorView extends IPublicModelPluginContext {}
+export interface IPublicModelEditorView extends IPublicModelPluginContext {
+  viewName: string;
+
+  viewType: 'editor' | 'webview';
+}
