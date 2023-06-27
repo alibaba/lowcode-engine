731dc58649ffa68a528c7b97e770f03e4252c2c8
liujuping <liujup@foxmail.com>
2023-06-27 14:07:07 +0800

    fix: lerna version to 4.0.0

--- packages/types/src/shell/type/node-schema.ts
+++ packages/types/src/shell/type/node-schema.ts
@@ -5,11 +5,14 @@ import { IPublicTypeCompositeValue, IPublicTypePropsMap, IPublicTypeNodeData } f
  * 搭建基础协议 - 单个组件树节点描述
  */
 export interface IPublicTypeNodeSchema {
+
   id?: string;
+
   /**
    * 组件名称 必填、首字母大写
    */
   componentName: string;
+
   /**
    * 组件属性对象
    */
@@ -18,25 +21,25 @@ export interface IPublicTypeNodeSchema {
   } & IPublicTypePropsMap; // | PropsList;
 
   /**
-   * 组件属性对象
-   */
-  leadingComponents?: string;
-  /**
    * 渲染条件
    */
   condition?: IPublicTypeCompositeValue;
+
   /**
    * 循环数据
    */
   loop?: IPublicTypeCompositeValue;
+
   /**
    * 循环迭代对象、索引名称 ["item", "index"]
    */
   loopArgs?: [string, string];
+
   /**
    * 子节点
    */
   children?: IPublicTypeNodeData | IPublicTypeNodeData[];
+
   /**
    * 是否锁定
    */
