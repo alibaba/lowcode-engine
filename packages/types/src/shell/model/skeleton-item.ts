9bb65cc95cb5bb1146dbbe01779d29cdfe3735fb
liujuping <liujup@foxmail.com>
2023-07-25 15:27:07 +0800

    feat: skeleton item add visible prop

--- packages/types/src/shell/model/skeleton-item.ts
+++ packages/types/src/shell/model/skeleton-item.ts
@@ -2,5 +2,15 @@
  * @since 1.1.7
  */
 export interface IPublicModelSkeletonItem {
+  name: string;
 
+  visible: boolean;
+
+  disable(): void;
+
+  enable(): void;
+
+  hide(): void;
+
+  show(): void;
 }
