79b9a6efa57d1a30559ea8476bbb380b5957d968
liujuping <liujup@foxmail.com>
2023-10-19 11:05:41 +0800

    feat: add theme color document & optimize some theme colors

--- packages/editor-skeleton/src/layouts/top-area.tsx
+++ packages/editor-skeleton/src/layouts/top-area.tsx
@@ -4,14 +4,14 @@ import { observer } from '@alilc/lowcode-editor-core';
 import { Area } from '../area';
 
 @observer
-export default class TopArea extends Component<{ area: Area; itemClassName?: string }> {
+export default class TopArea extends Component<{ area: Area; itemClassName?: string; className?: string }> {
   render() {
-    const { area, itemClassName } = this.props;
+    const { area, itemClassName, className } = this.props;
     if (area.isEmpty()) {
       return null;
     }
     return (
-      <div className={classNames('lc-top-area engine-actionpane', {
+      <div className={classNames(className, 'lc-top-area engine-actionpane', {
         'lc-area-visible': area.visible,
       })}
       >
