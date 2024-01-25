947c6218225e8b88df604cb3fa964e1298b18b84
liujuping <liujup@foxmail.com>
2024-01-25 15:47:48 +0800

    test(utils): add more test case for checkPropTypes

--- packages/utils/test/src/check-prop-types.test.ts
+++ packages/utils/test/src/check-prop-types.test.ts
@@ -12,6 +12,26 @@ describe('checkPropTypes', () => {
     expect(checkPropTypes('123', 'age', PropTypes.number, 'TestComponent')).toBe(false);
   });
 
+  it('should validate correctly with valid object prop type', () => {
+    expect(checkPropTypes({ a: 123 }, 'age', PropTypes.object, 'TestComponent')).toBe(true);
+    expect(checkPropTypes({ a: '123' }, 'age', PropTypes.object, 'TestComponent')).toBe(true);
+  });
+
+  it('should validate correctly with valid object string prop type', () => {
+    expect(checkPropTypes({ a: 123 }, 'age', 'object', 'TestComponent')).toBe(true);
+    expect(checkPropTypes({ a: '123' }, 'age', 'object', 'TestComponent')).toBe(true);
+  });
+
+  it('should validate correctly with valid isRequired prop type', () => {
+    const rule = {
+      type: 'string',
+      isRequired: true,
+    };
+    expect(transformPropTypesRuleToString(rule)).toBe('PropTypes.string.isRequired');
+    expect(checkPropTypes('News', 'type', rule, 'TestComponent')).toBe(true);
+    expect(checkPropTypes(undefined, 'type', rule, 'TestComponent')).toBe(false);
+  });
+
   it('should handle custom rule functions correctly', () => {
     const customRule = (props, propName) => {
       if (props[propName] !== 123) {
@@ -26,6 +46,11 @@ describe('checkPropTypes', () => {
   it('should interpret and validate a rule given as a string', () => {
     const result = checkPropTypes(123, 'age', 'PropTypes.number', 'TestComponent');
     expect(result).toBe(true);
+  });
+
+  it('should interpret and validate a rule given as a string', () => {
+    expect(checkPropTypes(123, 'age', 'number', 'TestComponent')).toBe(true);
+    expect(checkPropTypes('123', 'age', 'string', 'TestComponent')).toBe(true);
   });
 
   it('should log a warning for invalid rule type', () => {
@@ -58,6 +83,46 @@ describe('checkPropTypes', () => {
     expect(checkPropTypes('News', 'type', rule, 'TestComponent')).toBe(true);
     expect(checkPropTypes(123, 'type', rule, 'TestComponent')).toBe(true);
     expect(checkPropTypes({}, 'type', rule, 'TestComponent')).toBe(false);
+  });
+
+  it('should validate correctly with valid oneOfType prop type', () => {
+    const rule = {
+      type: 'oneOfType',
+      value: [
+        'bool',
+        {
+          type: 'shape',
+          value: [
+            {
+              name: 'type',
+              propType: {
+                type: 'oneOf',
+                value: ['JSExpression'],
+              }
+            },
+            {
+              name: 'value',
+              propType: 'string',
+            },
+          ],
+        },
+      ],
+    };
+    expect(transformPropTypesRuleToString(rule)).toBe('PropTypes.oneOfType([PropTypes.bool, PropTypes.shape({type: PropTypes.oneOf(["JSExpression"]),value: PropTypes.string})])');
+    expect(checkPropTypes(true, 'type', rule, 'TestComponent')).toBe(true);
+    expect(checkPropTypes({ type: 'JSExpression', value: '1 + 1 === 2' }, 'type', rule, 'TestComponent')).toBe(true);
+    expect(checkPropTypes({ type: 'JSExpression' }, 'type', rule, 'TestComponent')).toBe(true);
+    expect(checkPropTypes({ type: 'JSExpression', value: 123 }, 'type', rule, 'TestComponent')).toBe(false);
+  });
+
+  it('should log a warning for invalid type', () => {
+    const rule = {
+      type: 'inval',
+      value: ['News', 'Photos'],
+    }
+    expect(transformPropTypesRuleToString(rule)).toBe('PropTypes.any');
+    expect(checkPropTypes('News', 'type', rule, 'TestComponent')).toBe(true);
+    expect(checkPropTypes('Others', 'type', rule, 'TestComponent')).toBe(true);
   });
 
   // arrayOf
