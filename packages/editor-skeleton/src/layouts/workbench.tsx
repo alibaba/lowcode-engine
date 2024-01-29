339a48510d5fe8de8fe9d275523f9ca4fd2343cf
shunyun <ziscloud@gmail.com>
2024-01-29 15:03:08 +0800

    增加 split pane

--- packages/editor-skeleton/src/layouts/workbench.tsx
+++ packages/editor-skeleton/src/layouts/workbench.tsx
@@ -1,7 +1,7 @@
-import { Component } from 'react';
-import { TipContainer, observer } from '@alilc/lowcode-editor-core';
+import {Component} from 'react';
+import {TipContainer, observer} from '@alilc/lowcode-editor-core';
 import classNames from 'classnames';
-import { ISkeleton } from '../skeleton';
+import {ISkeleton} from '../skeleton';
 import TopArea from './top-area';
 import LeftArea from './left-area';
 import LeftFixedPane from './left-fixed-pane';
@@ -11,8 +11,14 @@ import MainArea from './main-area';
 import BottomArea from './bottom-area';
 import RightArea from './right-area';
 import './workbench.less';
-import { SkeletonContext } from '../context';
-import { EditorConfig, PluginClassSet } from '@alilc/lowcode-types';
+import {SkeletonContext} from '../context';
+import {EditorConfig, PluginClassSet} from '@alilc/lowcode-types';
+import SplitPane, {Pane} from "split-pane-react";
+import 'split-pane-react/esm/themes/default.css';
+
+interface SizeState {
+  sizes: (string | number)[];
+}
 
 @observer
 export class Workbench extends Component<{
@@ -21,11 +27,22 @@ export class Workbench extends Component<{
   components?: PluginClassSet;
   className?: string;
   topAreaItemClassName?: string;
-}> {
+}, SizeState> {
   constructor(props: any) {
     super(props);
-    const { config, components, skeleton } = this.props;
+    const {config, components, skeleton} = this.props;
     skeleton.buildFromConfig(config, components);
+    this.state = {sizes: ['70%', 'auto']}
+  }
+
+  setSizes(newsize: any[]) {
+    this.setState({sizes: newsize});
+  }
+
+  componentDidUpdate() {
+    console.log(document.querySelector('.lc-main-area')?.parentElement.parentElement)
+    const a =document.querySelector('.lc-main-area')?.parentElement.parentElement.style.width;
+    console.log('aaa', a)
   }
 
   render() {
@@ -37,19 +54,30 @@ export class Workbench extends Component<{
     return (
       <div className={classNames('lc-workbench', className)}>
         <SkeletonContext.Provider value={this.props.skeleton}>
-          <TopArea area={skeleton.topArea} itemClassName={topAreaItemClassName} />
+          <TopArea area={skeleton.topArea} itemClassName={topAreaItemClassName}/>
           <div className="lc-workbench-body">
-            <LeftArea area={skeleton.leftArea} />
-            <LeftFloatPane area={skeleton.leftFloatArea} />
-            <LeftFixedPane area={skeleton.leftFixedArea} />
-            <div className="lc-workbench-center">
-              <Toolbar area={skeleton.toolbar} />
-              <MainArea area={skeleton.mainArea} />
-              <BottomArea area={skeleton.bottomArea} />
-            </div>
-            <RightArea area={skeleton.rightArea} />
+            <LeftArea area={skeleton.leftArea}/>
+            <LeftFloatPane area={skeleton.leftFloatArea}/>
+            <LeftFixedPane area={skeleton.leftFixedArea}/>
+            <SplitPane
+              split='vertical'
+              sizes={this.state.sizes}
+              onChange={(sizes:(string|number)[])=>{
+                this.setSizes(sizes);
+              }}>
+              <Pane minSize={450} maxSize='100%'>
+                <div className="lc-workbench-center">
+                  <Toolbar area={skeleton.toolbar}/>
+                  <MainArea area={skeleton.mainArea}/>
+                  <BottomArea area={skeleton.bottomArea}/>
+                </div>
+              </Pane>
+              <Pane minSize={300} maxSize='50%'>
+                <RightArea area={skeleton.rightArea}/>
+              </Pane>
+            </SplitPane>
           </div>
-          <TipContainer />
+          <TipContainer/>
         </SkeletonContext.Provider>
       </div>
     );
---
+++ packages/engine/start-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ./dist
+python3 -m http.server 8081
