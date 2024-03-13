3ba09264388f707258c6272e395fa444f9d4cd3f
1ncounter <1ncounter.100@gmail.com>
2024-03-13 11:36:59 +0800

    chore: remove some useless files

--- index.ts
+++
@@ -1,7 +0,0 @@
-import renderer from './packages/react-simulator-renderer/src/renderer';
-
-if (typeof window !== 'undefined') {
-  (window as any).SimulatorRenderer = renderer;
-}
-
-export default renderer;
