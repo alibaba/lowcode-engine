1149044308fb204fd499869d2f5f449396dfefc4
1ncounter <1ncounter.100@gmail.com>
2024-04-03 14:23:23 +0800

    ci: change eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,34 @@
 import stylistic from '@stylistic/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint'
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/src/**/*.{ts?(x),js?(x)}'],
-    plugins: {
-      '@stylistic': stylistic,
-      'react-hooks': reactHooks,
-    },
-    languageOptions: {
-      parser: tsParser,
-    },
-    rules: {
-      '@stylistic/indent': ['error', 2],
-      "@stylistic/indent-binary-ops": ["error", 2],
-      '@stylistic/max-len': ["error", { "tabWidth": 2 }],
-      '@stylistic/no-tabs': "error",
-      '@stylistic/quotes': ["error", "single"],
-      "@stylistic/jsx-pascal-case": [2],
-      "@stylistic/jsx-indent": [2, 2, { checkAttributes: true, indentLogicalExpressions: true }],
+export default tseslint.config({
+  files: ['packages/*/src/**/*.{ts?(x),js?(x)}'],
+  ignores: ["**/*.test.ts"],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  plugins: {
+    '@stylistic': stylistic,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh
+  },
+  rules: {
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/indent-binary-ops': ['error', 2],
+    '@stylistic/max-len': ['error', { tabWidth: 2 }],
+    '@stylistic/no-tabs': 'error',
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/jsx-pascal-case': [2],
+    '@stylistic/jsx-indent': [2, 2, { checkAttributes: true, indentLogicalExpressions: true }],
+    '@stylistic/semi': ['error', 'always'],
 
-      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-      'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    },
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+
+    'react-refresh/only-export-components': 'warn',
   },
-  {
-    files: ['**/src/**/*.{tsx,jsx}'],
-    plugins: {
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      'react-refresh/only-export-components': 'warn',
-    },
-  },
-];
+})
